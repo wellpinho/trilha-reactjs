d2e4408e148b0347ae977f08ed9edc97a3b128bb
wellpinho <wellpinho@outlook.com>
2022-03-17 20:28:30 -0300

    ✨ NEW FEATURE: states

--- 01-github-explorer/src/App.jsx
+++ 01-github-explorer/src/App.jsx
@@ -1,8 +1,12 @@
+import Contador from './components/Contador';
 import { Repositorylist } from './components/RepositoryList';
 import './styles/global.scss'
 
 export const App = () => {
   return (
-    <Repositorylist />
+    <>
+      <Repositorylist />
+      <Contador />
+    </>
   );
 }
