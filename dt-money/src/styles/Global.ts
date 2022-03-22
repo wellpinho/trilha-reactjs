a5f5ee47b425c3d66bc3d2c050d37863a5cf2138
wellpinho <wellpinho@outlook.com>
2022-03-22 20:57:29 -0300

    ✨ NEW FEATURE: global style

---
+++ dt-money/src/styles/Global.ts
@@ -0,0 +1,48 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #f0f2f5;
+    --blue: #5429CC;
+    --blue-light: #6933ff;
+    --green: #33CC95;
+    --red: #E52E4D;
+    --text-title: #363f5f;
+    --text-body: #969cb3;
+    --shape: #FFFFFF;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  // font-siz: 16px; (ideal para desktop)
+  html { 
+    @media (max-width: 1080px) {
+      font-size: 93.75%; // 15px
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%; // 14px
+    }
+  }
+
+  // 1rem = font size da tela baseado na @media;
+
+  body { 
+    background: (--background);
+    // deixa a font mais nítidas
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity:0.6;
+    cursor: not-allowed;
+  }
+`
