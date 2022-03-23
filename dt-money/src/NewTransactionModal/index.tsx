6708718722b71a5e7ff8e4392f62c3878e2de0a6
wellpinho <wellpinho@outlook.com>
2022-03-23 10:26:41 -0300

    ✨ NEW FEATURE: form from modal and global styles

--- dt-money/src/NewTransactionModal/index.tsx
+++ dt-money/src/NewTransactionModal/index.tsx
@@ -1,22 +1,41 @@
-import Modal from 'react-modal'
+import Modal from 'react-modal';
 import { Container } from "./styles";
+
+import closeImg from './../assets/close.svg';
 
 interface INewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
 }
 // react modal sem isso fica acoplado ao elemento body
-Modal.setAppElement('#root')
+Modal.setAppElement('#root');
 
 export function NewTransactionModal({isOpen, onRequestClose}: INewTransactionModalProps) {
   return (
-    <Container>
       <Modal
+        overlayClassName="react-modal-overlay"
+        className="react-modal-content"
         isOpen={isOpen}
         onRequestClose={onRequestClose}
       >
-        Cadastrar transação
+        <button 
+          type="button" 
+          onClick={onRequestClose}
+          className="react-modal-close"
+        >
+          <img src={closeImg} alt="fechar modal" />
+        </button>
+
+        <Container>
+          <h2>Cadastrar transação</h2>
+
+          <form>
+            <input type="text" placeholder="Título" />
+            <input type="number" placeholder="Valor" />
+            <input type="number" placeholder="Categoria" />
+            <button type="submit">Cadastrar</button>
+          </form>
+        </Container>
       </Modal>
-    </Container>
   )
 }
