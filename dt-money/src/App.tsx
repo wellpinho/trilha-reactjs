996bce2fd6bb7e9d787be06f3ba84ea92ac8c8cd
wellpinho <wellpinho@outlook.com>
2022-03-23 09:24:25 -0300

    ✨ NEW FEATURE: react modal, states and props

--- dt-money/src/App.tsx
+++ dt-money/src/App.tsx
@@ -1,12 +1,36 @@
+import { useState } from 'react';
+import Modal from 'react-modal'
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/Global";
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false)
+
+  // quando for um evento click ou ação do user
+  // começar nome da função com handle...
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      {/* passando a função via props para o filho no caso index do component header */}
+      <Header onOpenNewTranslationModal={handleOpenNewTransactionModal} />
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+          Cadastrar transação
+      </Modal>
+
       <GlobalStyle />
     </>
   );
--- dt-money/src/components/Header/index.tsx
+++ dt-money/src/components/Header/index.tsx
@@ -1,13 +1,18 @@
 import Logo from '../../assets/logo.svg'
-
 import { Container, Content } from './styles'
 
-export function Header() {
+interface IHeaderProps {
+  onOpenNewTranslationModal: () => void;
+}
+
+export function Header({ onOpenNewTranslationModal }: IHeaderProps) {
   return (
     <Container>
       <Content>
         <img src={Logo} alt="dtmoney" />
-        <button type="button">
+
+        {/* props vinda do component App -> Header */}
+        <button onClick={onOpenNewTranslationModal} type="button">
           Nova transação
         </button>
       </Content>
